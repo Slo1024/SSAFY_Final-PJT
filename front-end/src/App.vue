35f7e115b899a3c397065b4199a34ae28dd5c7bc
jodndud <doriconi@gmail.com>
2025-05-15 23:49:50 +0900

    메인페이지 구성

--- front-end/src/App.vue
+++ front-end/src/App.vue
@@ -1,9 +1,39 @@
-<script setup>
-  import Navbar from './components/Navbar/Navbar.vue'
-</script>
-
 <template>
-  <div>
-    <Navbar />
+  <div class="container">
+    <LeftSection class="left-section" />
+    <RightSection class="right-section" />
   </div>
 </template>
+
+<script setup>
+import LeftSection from './components/home/LeftSection.vue'
+import RightSection from './components/home/RightSection.vue'
+</script>
+
+<style scoped>
+.container {
+  display: flex;flex-direction: column;
+  height: 100%;
+}
+
+.left-section, .right-section {
+  width: 100%;
+  display: flex;
+  padding: 0 50px;
+  justify-content: center;align-items: center;
+  box-sizing: border-box;
+}
+
+@media (min-width: 920px) {
+  .container {
+    flex-direction: row;
+  }
+  .left-section,
+  .right-section {
+    width: 50%;height: 100vh;
+  }
+  .right-section {
+    flex-direction: column;
+  }
+}
+</style>
---
+++ front-end/src/components/home/LeftSection.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="left-wrapper">
+    <div class="content">
+      <h1 class="title">책, 애매하게 기억나도 괜찮아요.</h1>
+      <p class="subtitle">유사도를 기반으로 하는 제목 교정 검색 서비스</p>
+      <input type="text" class="search-bar" placeholder="책 제목이나 키워드를 입력하세요" />
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.left-wrapper {
+  width: 100%;
+  display: flex;
+  justify-content: center; /* 가운데 정렬 */
+  align-items: center;
+}
+
+.content {
+  width: 100%;
+  text-align: center;
+}
+
+.title {
+  font-size: 2rem;
+  font-weight: bold;
+  margin-bottom: 1rem;
+}
+
+.subtitle {
+  font-size: 1rem;
+  color: #555;
+  margin-bottom: 2rem;
+}
+
+.search-bar {
+  width: 100%;
+  padding: 12px 16px;
+  font-size: 1rem;
+  border: 1px solid #ccc;
+  border-radius: 6px;
+  box-sizing: border-box;
+}
+</style>
